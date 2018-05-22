f40a0c29f03e27366455cf614792c1a21b4e6ada
max <mmaintz@posteo.de>
2018-05-22 09:12:37 +0200

    Highlight current thumbnail

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,22 @@
+
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert('moinsen');
+  // let thumbnails = document.querySelector("#gallery-thumbs").
+  //                           querySelectorAll("img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      let currentClass = 'current'
+      document.querySelector('.current').classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+    });
+  });
 }
